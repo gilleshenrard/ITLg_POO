0b9a5a9d56323d02def900342e04f96ff4b0e5f3
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-09 19:03:38 +0200

    add equals() and hashcode()

--- Awele/src/Models/Player.java
+++ Awele/src/Models/Player.java
@@ -1,6 +1,8 @@
 package Models;
 import Views.iSelectable;
 
+import java.util.Objects;
+
 public class Player {
     private int m_id;
     private String m_name;
@@ -42,5 +44,29 @@ public class Player {
      */
     public String getName() {
         return this.m_name;
+    }
+
+    /**
+     * Tells if o is equal to the current Player
+     * @param o Object to test
+     * @return true if equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return m_id == player.m_id &&
+                m_name.equals(player.m_name) &&
+                Objects.equals(m_behaviour, player.m_behaviour);
+    }
+
+    /**
+     * Returns the hashcode of the current Player
+     * @return Hashcode of the current Player
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(m_id, m_name, m_behaviour);
     }
 }
--- Awele/tests/Models/PlayerTest.java
+++ Awele/tests/Models/PlayerTest.java
@@ -5,11 +5,25 @@ import org.junit.jupiter.api.Test;
 
 class PlayerTest {
     Player p = new Player(1, "Testname");
+    Player p2 = new Player(1, "Testname");
+    Player p3 = new Player(2, "Testname");
+    Player p4 = new Player(1, "WrongName");
 
     @Test
     void setBehaviour_null_should_fail() {
         Assertions.assertThrows(NullPointerException.class, () -> {
             p.setBehaviour(null);
         });
+    }
+
+    @Test
+    void equals_shouldnot_fail(){
+        Assertions.assertEquals(p, p2);
+    }
+
+    @Test
+    void equals_should_fail(){
+        Assertions.assertNotEquals(p, p3);
+        Assertions.assertNotEquals(p, p4);
     }
 }
