9494de0bea84b2afcb1c8b987c2bbe5552b0c1fc
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-09 20:30:42 +0200

    implement Slot + unit tests

---
+++ Awele/src/Models/Slot.java
@@ -0,0 +1,49 @@
+package Models;
+
+public class Slot {
+    byte nb_seeds;
+
+    /**
+     * Create a new Slot
+     */
+    public Slot(){
+        this.nb_seeds = 4;
+    }
+
+    /**
+     * Return the amout of seeds in the slot
+     * @return Amount of seeds
+     */
+    public int getNbSeeds(){
+        return this.nb_seeds;
+    }
+
+    /**
+     * Set the amount of seeds to 0
+     */
+    public void emptySeeds(){
+        this.nb_seeds = 0;
+    }
+
+    /**
+     * Add one seed in the slot
+     * @throws IllegalStateException
+     */
+    public void incrementSeeds() throws IllegalStateException{
+        if(this.nb_seeds >= 24)
+            throw new IllegalStateException("A slot can have maximum 24 seeds");
+
+        this.nb_seeds += 1;
+    }
+
+    /**
+     * Remove one seed from the slot
+     * @throws IllegalStateException
+     */
+    public void decrementSeeds() throws IllegalStateException{
+        if (this.nb_seeds <= 0)
+            throw new IllegalStateException("The amount of seeds in a slot must be >= 0");
+
+        this.nb_seeds -= 1;
+    }
+}
---
+++ Awele/tests/Models/SlotTest.java
@@ -0,0 +1,51 @@
+package Models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SlotTest {
+    Slot s = new Slot();
+
+    @Test
+    void getNbSeeds_shouldnot_fail() {
+        Assertions.assertEquals(s.getNbSeeds(), 4);
+    }
+
+    @Test
+    void emptySeeds_shouldnot_fail() {
+        s.emptySeeds();
+        Assertions.assertEquals(s.getNbSeeds(), 0);
+    }
+
+    @Test
+    void incrementSeeds_shouldnot_fail() {
+        s.incrementSeeds();
+        Assertions.assertEquals(s.getNbSeeds(), 5);
+    }
+
+    @Test
+    void incrementSeeds_aboveMax_should_fail() {
+        for(int i=0 ; i<20 ; i++)
+            s.incrementSeeds();
+
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            s.incrementSeeds();
+        });
+    }
+
+    @Test
+    void decrementSeeds_shouldnot_fail() {
+        s.decrementSeeds();
+        Assertions.assertEquals(s.getNbSeeds(), 3);
+    }
+
+    @Test
+    void decrementSeeds_negativeamount_should_fail() {
+        s.emptySeeds();
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            s.decrementSeeds();
+        });
+    }
+}
