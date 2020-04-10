36314c9e99e58fda43ddf4e094666724833038bd
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-10 21:23:16 +0200

    implement playSlot() and getSumCapturable

--- Awele/src/Models/Board.java
+++ Awele/src/Models/Board.java
@@ -1,6 +1,7 @@
 package Models;
 
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
 public class Board {
     private Slot[][] m_slots;
@@ -92,5 +93,91 @@ public class Board {
         }
 
         return this.getSlot(x, y);
+    }
+
+    /**
+     * Harvest the seeds from a slot and, if necessary, scatter them
+     * @param id ID of the player harvesting
+     * @param slot Slot being harvested
+     * @return 0 if no futher action, 1 if victory, 2 if season cancelled
+     * @throws InvalidParameterException
+     */
+    public int playSlot(int id, int slot) throws InvalidParameterException{
+        if(id != 1 && id != 2)
+            throw new InvalidParameterException("The ID must be 1 or 2");
+
+        if(slot < 1 || slot > 6)
+            throw new InvalidParameterException("The slot chosen must be between 1 and 6 included");
+
+        //get number of seeds in the slot harvested by the player
+        Slot s = this.getSlot(slot-1, id-1);
+        int nbseeds = s.getNbSeeds();
+        int backupseeds = s.getNbSeeds();
+
+        //empty the slot (it's been harvested)
+        s.emptySeeds();
+
+        //save all the slots after the slot harvested in a buffer until there are no seeds left
+        Slot sNext = this.getNext(s);
+        ArrayList<Slot> buffer = new ArrayList<Slot>();
+        while (nbseeds > 0){
+            //make sure not to add the slot harvested
+            if(!sNext.equals(s)) {
+                sNext.incrementSeeds();
+                buffer.add(sNext);
+                nbseeds--;
+            }
+
+            sNext = this.getNext(sNext);
+        }
+
+        //get the amount of seeds in the last slot of the buffer
+        int seeds = buffer.get(buffer.size()-1).getNbSeeds();
+
+        //if that amount is 2 or 3, a capture needs to be made
+        if(seeds == 2 || seeds == 3){
+            //check if this seasons risks to starve the opponent
+            int sumseeds = this.getSumCapturable(buffer);
+            if((id == 1 && sumseeds == this.remSeedsPl2) || (id == 2 && sumseeds == this.remSeedsPl1)){
+                //opponent starved, cancellation
+                for (Slot tmp:buffer) {
+                    tmp.decrementSeeds();
+                }
+                s.setNbSeeds(backupseeds);
+
+                return 2;
+            }
+            else{
+                //opponent not starved, capture
+                for (Slot tmp:buffer) {
+                    this.m_game.storeSeeds(id, tmp.nb_seeds);
+                    tmp.emptySeeds();
+                }
+            }
+
+            if(this.m_game.getSeeds(id) > 24)
+                return 1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Get the sum of seeds which can be captured in the buffer
+     * @param buffer Buffer from which get the sum
+     * @return Sum of the seeds which can be captured
+     * @throws NullPointerException
+     */
+    private int getSumCapturable(ArrayList<Slot> buffer) throws NullPointerException{
+        if(buffer == null)
+            throw new NullPointerException("buffer must be instantiated");
+
+        int total = 0;
+        for (Slot s: buffer) {
+            if(s.getNbSeeds() == 2 || s.getNbSeeds()==3)
+                total += s.getNbSeeds();
+        }
+
+        return total;
     }
 }
