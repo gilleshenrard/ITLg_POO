34524dbebabd5f2a4ca9f296a7039691e11a4b55
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-10 10:36:53 +0200

    make displayWin use Game.getName()

--- Awele/src/Views/GameView.java
+++ Awele/src/Views/GameView.java
@@ -1,17 +1,26 @@
 package Views;
 
+import Models.Game;
+
 import java.security.InvalidParameterException;
 
 public class GameView {
+    Game m_game;
+
+    /**
+     * Create a new Game View
+     */
+    public GameView(){
+        this.m_game = Game.getInstance();
+    }
+
     /**
      * Tell which player won the game
      * @param ID ID of the player
      * @throws InvalidParameterException
+     * @throws NullPointerException
      */
-    public void displayWin(int ID) throws InvalidParameterException{
-        if(ID != 1 && ID !=2)
-            throw new InvalidParameterException("The ID must be 1 or 2");
-
-        System.out.println("Player " + ID + " won the game !");
+    public void displayWin(int ID) throws InvalidParameterException, NullPointerException{
+        System.out.println(this.m_game.getName(ID) + " won the game !");
     }
 }
