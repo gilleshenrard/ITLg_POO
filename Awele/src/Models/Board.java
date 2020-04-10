17baa3cf4e35791cdfa5a1be7f6187bbd76974db
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-10 20:04:21 +0200

    implement getSlot() and getNext() + unit tests

--- Awele/src/Models/Board.java
+++ Awele/src/Models/Board.java
@@ -54,4 +54,43 @@ public class Board {
         else
             return this.remSeedsPl2;
     }
+
+    /**
+     * Get the slot located at X,Y
+     * @param x X coordinate of the slot
+     * @param y Y coordinate of the slot
+     * @return Slot requested
+     * @throws InvalidParameterException
+     */
+    public Slot getSlot(int x, int y) throws InvalidParameterException{
+        if(x < 0 || x > 5)
+            throw new InvalidParameterException("X value must be 0 <= x < 6");
+        if(y != 0 && y != 1)
+            throw new InvalidParameterException("Y value must be 0 or 1");
+
+        return this.m_slots[y][x];
+    }
+
+    /**
+     * Get the next slot (increment x, and roll y when reached the end)
+     * @param s Slot of which to find the next
+     * @return Next slot to s
+     * @throws NullPointerException
+     */
+    public Slot getNext(Slot s) throws NullPointerException{
+        if (s == null)
+            throw new NullPointerException("Slot must not be null");
+
+        int x = s.getX();
+        int y = s.getY();
+
+        x++;
+        x %= 6;
+        if(x == 0){
+            y++;
+            y %= 2;
+        }
+
+        return this.getSlot(x, y);
+    }
 }
--- Awele/tests/Models/BoardTest.java
+++ Awele/tests/Models/BoardTest.java
@@ -43,4 +43,72 @@ class BoardTest {
     void getRemainingSeeds_shouldnot_fail() {
         Assertions.assertEquals(b.getRemainingSeeds(1), 24);
     }
+
+    /**
+     * Check if getSlot() throws an exception with an X value over 5
+     */
+    @Test
+    void getSlot_Xover5_should_fail() {
+        Assertions.assertThrows(InvalidParameterException.class, () -> {
+            b.getSlot(6, 0);
+        });
+    }
+
+    /**
+     * Check if getSlot() throws an exception with an invalid Y value
+     */
+    @Test
+    void getSlot_invalidY_should_fail() {
+        Assertions.assertThrows(InvalidParameterException.class, () -> {
+            b.getSlot(0, 3);
+        });
+    }
+
+    /**
+     * Check if getSlot() throws an exception with an X value below 0
+     */
+    @Test
+    void getSlot_Xbelow0_should_fail() {
+        Assertions.assertThrows(InvalidParameterException.class, () -> {
+            b.getSlot(-1, 0);
+        });
+    }
+
+    /**
+     * Check if getRemainingSeeds() returns the proper coordinates for next slot (within a row)
+     */
+    @Test
+    void getNext_withinARow_shouldnot_fail() {
+        Slot s = b.getSlot(2, 0);
+        Assertions.assertEquals(b.getNext(s).getX(), 3);
+        Assertions.assertEquals(b.getNext(s).getY(), 0);
+    }
+
+    /**
+     * Check if getRemainingSeeds() returns the proper coordinates for next slot (end of the first row)
+     */
+    @Test
+    void getNext_endOfFirstRow_shouldnot_fail() {
+        Slot s = b.getSlot(5, 0);
+        Assertions.assertEquals(b.getNext(s).getX(), 0);
+        Assertions.assertEquals(b.getNext(s).getY(), 1);
+    }
+
+    /**
+     * Check if getRemainingSeeds() returns the proper coordinates for next slot (end of the 2nd row)
+     */
+    @Test
+    void getNext_endOfSecondRow_shouldnot_fail() {
+        Slot s = b.getSlot(5, 1);
+        Assertions.assertEquals(b.getNext(s).getX(), 0);
+        Assertions.assertEquals(b.getNext(s).getY(), 0);
+    }
+
+    /**
+     * Check if getSlot() fails returning a slot
+     */
+    @Test
+    void getSlot_shouldnot_fail() {
+        b.getSlot(0, 0);
+    }
 }
