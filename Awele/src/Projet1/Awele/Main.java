56f98ca5f5c49d7ce136e9f7a2d1c912ffd5f10f
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-11 10:23:04 +0200

    implement rest of the game loop

--- Awele/src/Projet1/Awele/Main.java
+++ Awele/src/Projet1/Awele/Main.java
@@ -5,25 +5,52 @@ import Models.Board;
 import Models.Game;
 import Models.Player;
 import Views.BoardView;
+import Views.GameView;
 import Views.KeyboardSelect;
+
+import java.security.InvalidParameterException;
 
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game();
+        Game game = Game.getInstance();
         game.setPlayer(1, new Player(1, "Gilles", new KeyboardSelect()));
         game.setPlayer(2, new Player(2, "AI", new RandomSelect()));
         game.setBoard(new Board());
         BoardView b = new BoardView(game.getBoard());
+        GameView gameView = new GameView();
         int choice, player = 0, outcome = 0;
 
-        while (true) {
+        //main game loop
+        while (outcome != 1) {
             b.displayBoard();
+
+            //loop while the user selects a wrong slot or the opponent risks being starved
             do {
-                choice = game.getPlayer(player + 1).selectSlot();
-                game.getBoard().playSlot(player + 1, choice);
+                try {
+                    choice = game.getPlayer(player + 1).selectSlot();
+                    outcome = game.playSlot(player + 1, choice);
+
+                    //player starved
+                    if (outcome == 2)
+                        gameView.displayWarning("A player can't be starved. Its amount of seeds can't get to 0");
+                }
+                //System error. Display error message
+                catch (NullPointerException e){
+                    gameView.displayError(e.getMessage());
+                }
+                //Slot out of range or other system error. Display message and get back in the loop
+                catch (InvalidParameterException e){
+                    gameView.displayError(e.getMessage());
+                    outcome = 2;
+                }
             }while (outcome == 2);
 
+            //Game is won by the current player.
+            if (outcome == 1)
+                gameView.displayMessage(game.getName(player+1) + " won the game !");
+
+            //Select next player
             player++;
             player %= 2;
         }
