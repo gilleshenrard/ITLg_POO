90233995dbb0734b283715a5b0ecdcd0b5e8709d
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-13 18:12:35 +0200

    make RandomSelect avoid collisions + unit tests

--- Awele/src/Views/RandomSelect.java
+++ Awele/src/Views/RandomSelect.java
@@ -1,16 +1,57 @@
 package Views;
 
+import Models.Board;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomSelect implements iSelectable {
+    private int m_id;
+    private Board m_board;
+    private ArrayList<Integer> m_playable;
+
+    public RandomSelect(Board board, int ID){
+        this.m_id = ID;
+        this.m_board = board;
+        this.m_playable = null;
+    }
+
     /**
      * Randomly select a slot (between 1 and 6)
      * @return Random slot number
+     * @throws NullPointerException
      */
     @Override
-    public int selectSlot(){
+    public int selectSlot() throws NullPointerException{
+        if (this.m_playable == null)
+            throw new NullPointerException("RandomSelect.selectSlot() : null instance of ArrayList<Integer>");
 
-        Random r = new Random();
-        return 1 + r.nextInt(6);
+        if (this.m_playable.size() > 0) {
+            Random r = new Random();
+            Integer index = this.m_playable.get(r.nextInt(this.m_playable.size()));
+            this.m_playable.remove(index);
+            return 1 + index.intValue();
+        }
+        else
+            return 0;
+    }
+
+    /**
+     * Refresh the array of non-empty slots
+     */
+    public void reset(){
+        this.m_playable = this.m_board.getNonEmpty(this.m_id);
+    }
+
+    /**
+     * Return the amount of slots not tried yet
+     * @return Amount of slots left
+     * @throws NullPointerException
+     */
+    public int getShotsLeft() throws NullPointerException{
+        if (this.m_playable == null)
+            throw new NullPointerException("RandomSelect.selectSlot() : null instance of ArrayList<Integer>");
+
+        return this.m_playable.size();
     }
 }
--- Awele/tests/Views/RandomSelectTest.java
+++ Awele/tests/Views/RandomSelectTest.java
@@ -1,11 +1,13 @@
 package Views;
 
+import Models.Board;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RandomSelectTest {
-    RandomSelect r = new RandomSelect();
+    Board b = new Board();
+    RandomSelect r = new RandomSelect(b, 1);
 
     /**
      * Verify that selectSlot() generates 0 <= value <= 5
@@ -13,7 +15,49 @@ class RandomSelectTest {
     @DisplayName("selectSlot() - should not fail")
     @Test
     void selectSlot_shouldnot_fail() {
+        r.reset();
         int buffer = r.selectSlot();
         Assertions.assertTrue(buffer >0 && buffer <=6);
+    }
+
+    /**
+     * Verify that selectSlot() throws an exception with a NULL instance
+     */
+    @DisplayName("selectSlot() with m_playable NULL - should fail")
+    @Test
+    void selectSlot_NULL_should_fail() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            int buffer = r.selectSlot();
+        });
+    }
+
+    /**
+     * Verify that reset() generates 0 <= value <= 5
+     */
+    @DisplayName("reset() - should not fail")
+    @Test
+    void reset_shouldnot_fail() {
+        r.reset();
+    }
+
+    /**
+     * Verify that getShotsLeft() returns the proper value
+     */
+    @DisplayName("getShotsLeft() - should not fail")
+    @Test
+    void getShotsLeft_shouldnot_fail() {
+        r.reset();
+        Assertions.assertEquals(6, r.getShotsLeft());
+    }
+
+    /**
+     * Verify that getShotsLeft() throws an exception with a NULL instance
+     */
+    @DisplayName("getShotsLeft() with m_playable NULL - should fail")
+    @Test
+    void getShotsLeft_NULL_should_fail() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            r.getShotsLeft();
+        });
     }
 }
