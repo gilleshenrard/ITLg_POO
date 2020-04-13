0302808aeac2d286e9f463db525bcf72b5e92cd5
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-13 09:28:48 +0200

    implement setPlayableSlots() + unit tests

--- Awele/src/Views/RandomSelect.java
+++ Awele/src/Views/RandomSelect.java
@@ -1,25 +1,40 @@
 package Views;
 
-import Views.iSelectable;
-
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomSelect implements iSelectable {
-    private Random m_rand;
+    private ArrayList<Integer> m_playableSlots;
 
     /**
      * Create a new random slot selector
      */
     public RandomSelect(){
-        this.m_rand = new Random();
+        this.m_playableSlots = null;
     }
 
     /**
      * Randomly select a slot (between 1 and 6)
      * @return Random slot number
+     * @throws NullPointerException
      */
     @Override
-    public int selectSlot() {
-        return 1 + this.m_rand.nextInt(6);
+    public int selectSlot() throws NullPointerException{
+        if (this.m_playableSlots == null)
+            throw new NullPointerException("RandomSelect.selectSlot() : null instance of ArrayList<Integer>");
+
+        Random r = new Random();
+        return 1 + this.m_playableSlots.get(r.nextInt(this.m_playableSlots.size()));
+    }
+
+    /**
+     * Inform RandomSelect about the possible slots to play
+     * @param playable Slot possible to be picked
+     * @throws NullPointerException
+     */
+    public void setPlayableSlots(ArrayList<Integer> playable) throws NullPointerException {
+        if (playable == null)
+            throw new NullPointerException("RandomSelect.setPlayableSlots() : NULL instance of ArrayList<Integer>");
+        this.m_playableSlots = playable;
     }
 }
--- Awele/tests/Views/RandomSelectTest.java
+++ Awele/tests/Views/RandomSelectTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+
 class RandomSelectTest {
     RandomSelect r = new RandomSelect();
 
@@ -13,7 +16,41 @@ class RandomSelectTest {
     @DisplayName("selectSlot() - should not fail")
     @Test
     void selectSlot_shouldnot_fail() {
+        ArrayList<Integer> test = new ArrayList<>();
+        test.add(2);
+        r.setPlayableSlots(test);
         int buffer = r.selectSlot();
         Assertions.assertTrue(buffer >0 && buffer <=6);
+    }
+
+    /**
+     * Verify that selectSlot() throws an exception with a NULL instance
+     */
+    @DisplayName("selectSlot() with a null instance - should fail")
+    @Test
+    void selectSlot_null_should_fail() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            r.selectSlot();
+        });
+    }
+
+    /**
+     * Verify that setPlayableSlots() properly sets the array of possible slots
+     */
+    @DisplayName("setPlayableSlots() - should not fail")
+    @Test
+    void setPlayableSlots_shouldnot_fail() {
+        r.setPlayableSlots(new ArrayList<>());
+    }
+
+    /**
+     * Verify that setPlayableSlots() throws an exception with a NULL instance
+     */
+    @DisplayName("setPlayableSlots() with a null instance - should fail")
+    @Test
+    void setPlayableSlots_null_should_fail() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            r.setPlayableSlots(null);
+        });
     }
 }
