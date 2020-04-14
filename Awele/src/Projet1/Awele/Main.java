59f3ea8dec247fd25576aa0602a55f3e771a3b22
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-14 08:23:15 +0200

    simplify game loop + add comments

--- Awele/src/Projet1/Awele/Main.java
+++ Awele/src/Projet1/Awele/Main.java
@@ -14,12 +14,8 @@ public class Main {
     public static void main(String[] args) {
         Game game = Game.getInstance();
         game.setBoard(new Board());
-        game.setPlayer(1, new Player(1, "Gilles"));
-        game.setPlayer(2, new Player(2, "AI"));
-        RandomSelect r1 = new RandomSelect(game.getBoard(), 1);
-        game.getPlayer(1).setBehaviour(r1);
-        RandomSelect r2 = new RandomSelect(game.getBoard(), 2);
-        game.getPlayer(2).setBehaviour(r2);
+        game.setPlayer(1, new Player(1, "Gilles", new RandomSelect(game.getBoard(), 1)));
+        game.setPlayer(2, new Player(2, "AI", new RandomSelect(game.getBoard(), 2)));
         BoardView b = new BoardView(game.getBoard());
         GameView gameView = new GameView();
         int choice, player = 0, outcome = 0;
@@ -29,37 +25,52 @@ public class Main {
             gameView.displayMessage("This is " + game.getName(player+1) + "'s season");
             b.displayBoard();
 
-            if(r1 != null){
-                r1.reset();
+            //refresh playable slots for player 1 if he plays randomly
+            if(game.getPlayer(1).getBehaviour() instanceof RandomSelect){
+                RandomSelect r = (RandomSelect) game.getPlayer(1).getBehaviour();
+                r.reset();
             }
 
-            if(r2 != null){
-                r2.reset();
+            //refresh playable slots for player 2 if he plays randomly
+            if(game.getPlayer(2).getBehaviour() instanceof RandomSelect){
+                RandomSelect r = (RandomSelect) game.getPlayer(2).getBehaviour();
+                r.reset();
             }
 
             //loop while the user selects a wrong slot or the opponent risks being starved
             do {
                 try {
+                    //select a slot, then play it
                     choice = game.getPlayer(player + 1).selectSlot();
-                    gameView.displayMessage(game.getName(player+1) + " harvests the slot " + choice);
+                    gameView.displayMessage(game.getName(player + 1) + " harvests the slot " + choice);
                     outcome = game.playSlot(player + 1, choice);
 
-                    //player starved
-                    if (outcome == 2)
-                        gameView.displayWarning("A player can't be starved. Its amount of seeds can't get to 0");
+                    //in case of starvation or empty slot played
+                    if (outcome == 2 || outcome == 3) {
+                        //player starved
+                        if (outcome == 2)
+                            gameView.displayWarning("A player can't be starved. Its amount of seeds can't get to 0");
 
-                    //player starved
-                    if (outcome == 3)
-                        gameView.displayWarning("An empty slot can not be harvested");
+                        //empty slot played
+                        if (outcome == 3)
+                            gameView.displayWarning("An empty slot can not be harvested");
 
-                    if(outcome == 2 || outcome == 3){
-                        if (r1 != null && r1.getShotsLeft() == 0){
-                            gameView.displayMessage(game.getName(1) + " can't make any move. He forfaits !");
-                            System.exit(0);
+                        //if player 1 plays randomly and don't have any possible moves left, forfeit
+                        if (game.getPlayer(1).getBehaviour() instanceof RandomSelect) {
+                            RandomSelect r = (RandomSelect) game.getPlayer(1).getBehaviour();
+                            if (r.getShotsLeft() == 0) {
+                                gameView.displayMessage(game.getName(1) + " can't make any move. He forfeits !");
+                                System.exit(0);
+                            }
                         }
-                        if (r2 != null && r2.getShotsLeft() == 0){
-                            gameView.displayMessage(game.getName(2) + " can't make any move. He forfaits !");
-                            System.exit(0);
+
+                        //if player 2 plays randomly and don't have any possible moves left, forfeit
+                        if (game.getPlayer(2).getBehaviour() instanceof RandomSelect) {
+                            RandomSelect r = (RandomSelect) game.getPlayer(2).getBehaviour();
+                            if (r.getShotsLeft() == 0) {
+                                gameView.displayMessage(game.getName(2) + " can't make any move. He forfeits !");
+                                System.exit(0);
+                            }
                         }
                     }
                 }
@@ -74,14 +85,6 @@ public class Main {
                     System.exit(-2);
                 }
             }while (outcome == 2 || outcome == 3);
-
-            if(r1 != null){
-                r1.reset();
-            }
-
-            if(r2 != null){
-                r2.reset();
-            }
 
             gameView.displayMessage("--------------------------------------------------------------------");
 
