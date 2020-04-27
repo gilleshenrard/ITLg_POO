131e314759c0c1640cb5689cd7b739e8c9cdf2d0
Gilles Henrard <gilles.henrard@hotmail.com>
2020-04-27 10:29:04 +0200

    move outcome handling to a separate private method for readability

--- Awele/src/Controllers/PlayingState.java
+++ Awele/src/Controllers/PlayingState.java
@@ -30,37 +30,45 @@ public class PlayingState implements iGameState {
             return -1;
         }
 
-        if (outcome < 0) {
-            //player starved
-            if (outcome == -1)
-                controller.displayWarning("A player can't be starved. Its amount of seeds can't get to 0");
-
-            //empty slot played
-            if (outcome == -2)
-                controller.displayWarning("An empty slot can not be harvested");
-
-            //if current player plays randomly and don't have any possible moves left, forfeit
-            if (controller.isPlayerAI(controller.getCurrentPlayer())) {
-                if (controller.getShotsLeft(controller.getCurrentPlayer()) == 0) {
-                    controller.displayMessage(controller.getName(controller.getCurrentPlayer()) + " can't make any move. He forfeits !");
-
-                    //Easter egg : when both players play randomly and one of them forfeits, he says the last quote of the W.P.O.R. in the movie Wargames
-                    if(controller.isPlayerAI(1) && controller.isPlayerAI(2)) {
-                        controller.displayMessage("\n" + controller.getName(controller.getCurrentPlayer()) + " : 'A strange game... The only winning move is not to play...'");
-                        controller.displayMessage(controller.getName(controller.getCurrentPlayer()) + " : '......................... How about a nice game of chess?'");
-                    }
-                    return -2;
-                }
-            }
-            //get back to the Prompting state
-            controller.setNextState(controller.m_prompting);
-
-            return 0;
-        }
+        if (outcome < 0)
+            return handleOutcome(controller, outcome);  //player starved or empty slot
         else
-            //Go to the Storing state
-            controller.setNextState(controller.m_storing);
+            controller.setNextState(controller.m_storing);  //Go to the Storing state
 
         return outcome;
+    }
+
+    /**
+     * Handle any starving or empty slot outcome
+     * @param controller Game controller to use
+     * @param outcome Outcome of the current season played
+     * @return Value to return to the main loop (-2 if forfeit, 0 if ok)
+     */
+    private int handleOutcome(GameController controller, int outcome){
+        //player starved
+        if (outcome == -1)
+            controller.displayWarning("A player can't be starved. Its amount of seeds can't get to 0");
+
+        //empty slot played
+        if (outcome == -2)
+            controller.displayWarning("An empty slot can not be harvested");
+
+        //if current player plays randomly and don't have any possible moves left, forfeit
+        if (controller.isPlayerAI(controller.getCurrentPlayer())) {
+            if (controller.getShotsLeft(controller.getCurrentPlayer()) == 0) {
+                controller.displayMessage(controller.getName(controller.getCurrentPlayer()) + " can't make any move. He forfeits !");
+
+                //Easter egg : when both players play randomly and one of them forfeits, he says the last quote of the W.P.O.R. in the movie Wargames
+                if(controller.isPlayerAI(1) && controller.isPlayerAI(2)) {
+                    controller.displayMessage("\n" + controller.getName(controller.getCurrentPlayer()) + " : 'A strange game... The only winning move is not to play...'");
+                    controller.displayMessage(controller.getName(controller.getCurrentPlayer()) + " : '......................... How about a nice game of chess?'");
+                }
+                return -2;
+            }
+        }
+        //get back to the Prompting state
+        controller.setNextState(controller.m_prompting);
+
+        return 0;
     }
 }
