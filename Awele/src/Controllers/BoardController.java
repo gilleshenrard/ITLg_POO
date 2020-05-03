43dc4f05e791aa98620bf5e3665dae1fc8c6df7a
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-03 18:55:04 +0200

    fix isLegal() bugs

--- Awele/src/Controllers/BoardController.java
+++ Awele/src/Controllers/BoardController.java
@@ -83,30 +83,41 @@ public class BoardController {
         if (nbseeds == 0)
             return -2;
 
-        //backup the amount of seeds in the slot harvested by the player
+        //prepare buffer variables
+        Point tmp = new Point(p);
         int[] backup = new int[2];
+        int[] capturable = new int[2];
+        int[] scattered = new int[2];
+
+        //backup the amount of seeds in the slot harvested by the player
         backup[0] = (p.getY() == 0 ? nbseeds : 0);
         backup[1] = (p.getY() == 1 ? nbseeds : 0);
 
         //compute the amount of seeds which would be captured in each player row
         //  (1 or 2 in each slot in which the tested slot would be scattered)
-        Point tmp = new Point(p);
-        int[] capturable = new int[2];
+        java.util.Arrays.fill(scattered, 0);
         java.util.Arrays.fill(capturable, 0);
         do {
             tmp = this.m_board.getNext(tmp);
             if (this.getSlotSeeds(tmp) == 1 || this.getSlotSeeds(tmp) == 2)
                 capturable[tmp.getY()] += getSlotSeeds(tmp);
+            else
+                scattered[tmp.getY()]++;
             nbseeds--;
         }while (nbseeds > 0);
 
-        //starvation case for the player 1
-        if(backup[0] + (this.getSlotSeeds(tmp) == 1 || this.getSlotSeeds(tmp) == 2 ? capturable[0] : 0) == this.m_board.getRemainingSeeds(1))
-            return -1;
-
-        //starvation case for the player 2
-        if(backup[1] + (this.getSlotSeeds(tmp) == 1 || this.getSlotSeeds(tmp) == 2 ? capturable[1] : 0) == this.m_board.getRemainingSeeds(2))
-            return -1;
+        //starvation occurring during a capture
+        if(this.getSlotSeeds(tmp) == 1 || this.getSlotSeeds(tmp) == 2){
+            if(capturable[0] + backup[0] == this.m_board.getRemainingSeeds(1)
+               || capturable[1] + backup[1] == this.m_board.getRemainingSeeds(2))
+                return -1;
+        }
+        //starvation occurring during a scattering
+        else {
+            if(scattered[0] + backup[0] == this.m_board.getRemainingSeeds(1)
+               || scattered[1] + backup[1] == this.m_board.getRemainingSeeds(2))
+                return -1;
+        }
 
         return 1;
     }
