731379c35a83d7be2b08acb342640b6cd40bc050
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-03 12:20:20 +0200

    update PlayingState unit tests

--- Awele/tests/Controllers/PlayingStateTest.java
+++ Awele/tests/Controllers/PlayingStateTest.java
@@ -53,12 +53,13 @@ public class PlayingStateTest {
     /**
      * Check if handleState() processes a starvation properly
      */
-    @DisplayName("handleState() with a starvation case - should not fail")
+    @DisplayName("handleState() with a starvation case, no forfeit - should not fail")
     @Test
-    void handleState_noCaptureStarve_shouldnot_fail() {
+    void handleState_noCaptureStarveNoForfeit_shouldnot_fail() {
         Game.getInstance().setSeeds(1, 0);
         Game.getInstance().setSeeds(2, 0);
-        g.setBoardController(b);
+        Game.getInstance().setPlayer(new Player(1, "Test", new RandomSelect(b, 1)));
+        b.resetBoard();
         b.getBoard().setSlotSeeds(new Point(0, 1), 1);
         b.getBoard().setSlotSeeds(new Point(1, 1), 2);
         b.getBoard().emptySlotSeeds(new Point(2, 1));
@@ -72,34 +73,16 @@ public class PlayingStateTest {
         g.setCurrentPlayer(1);
         g.refresh(1);
         int ret = g.handleState(6);
-        Assertions.assertEquals(-1, ret);
+        Assertions.assertEquals(0, ret);
         Assertions.assertTrue(g.getNextState() instanceof PromptingState);
-    }
-
-    /**
-     * Check if handleState() processes a victory season
-     */
-    @DisplayName("handleState() with a victory case - should not fail")
-    @Test
-    void handleState_victory_shouldnot_fail() {
-        g.setBoardController(b);
-        b.getBoard().setSlotSeeds(new Point(3, 1), 1);
-        b.getBoard().setSlotSeeds(new Point(1, 1), 2);
-        Game.getInstance().setSeeds(2, 0);
-        Game.getInstance().setSeeds(1, 20);
-        g.setNextState(GameController.m_playing);
-        g.setCurrentPlayer(1);
-        int ret = g.handleState(6);
-        Assertions.assertEquals(5, ret);
-        Assertions.assertTrue(g.getNextState() instanceof StoringState);
     }
 
     /**
      * Check if handleState() forbids a self-starvation by scattering to another row
      */
-    @DisplayName("handleState() with self-starvation to other row - should not fail")
+    @DisplayName("handleState() with self-starvation to other row, forfeit - should not fail")
     @Test
-    void handleState_selfStarvation_otherRow_shouldnot_fail() {
+    void handleState_selfStarvationForfeit_otherRow_shouldnot_fail() {
         Game.getInstance().setSeeds(1, 0);
         Game.getInstance().setSeeds(2, 0);
         Game.getInstance().setPlayer(new Player(1, "Test", new RandomSelect(b, 1)));
@@ -123,12 +106,15 @@ public class PlayingStateTest {
     /**
      * Check if handleState() forbids a self-starvation by scattering within a row
      */
-    @DisplayName("handleState() with self-starvation within a row - should not fail")
+    @DisplayName("handleState() with self-starvation within a row, no forfeit - should not fail")
     @Test
-    void handleState_selfStarvation_sameRow_shouldnot_fail() {
+    void handleState_selfStarvationNoForfeit_sameRow_shouldnot_fail() {
         Game.getInstance().setSeeds(1, 0);
         Game.getInstance().setSeeds(2, 0);
+        Game.getInstance().setPlayer(new Player(1, "Test", new RandomSelect(b, 1)));
+        Game.getInstance().setPlayer(new Player(2, "Test", new RandomSelect(b, 2)));
         g.setBoardController(b);
+        b.resetBoard();
         b.getBoard().emptySlotSeeds(new Point(0, 0));
         b.getBoard().emptySlotSeeds(new Point(1, 0));
         b.getBoard().emptySlotSeeds(new Point(2, 0));
@@ -140,7 +126,7 @@ public class PlayingStateTest {
         g.setNextState(GameController.m_playing);
         g.refresh(1);
         int ret = g.handleState(5);
-        Assertions.assertEquals(-2, ret);
+        Assertions.assertEquals(0, ret);
         Assertions.assertTrue(g.getNextState() instanceof PromptingState);
     }
 }
