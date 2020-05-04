02ff87b67ba97b32b98befcebf80bf0387a4c9d9
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-04 18:24:26 +0200

    refine minimax

--- Awele/src/Views/MinimaxSelect.java
+++ Awele/src/Views/MinimaxSelect.java
@@ -1,9 +1,13 @@
 package Views;
 
 import Controllers.BoardController;
+import Models.Board;
+import Models.Point;
+
+import java.util.ArrayList;
 
 public class MinimaxSelect implements iSelectable{
-    private BoardController m_board;
+    private BoardController m_controller;
     private int m_id;
     private int m_maxDepth;
     private static int ERROR = -100000;
@@ -14,13 +18,13 @@ public class MinimaxSelect implements iSelectable{
      * @param ID The ID of the player to which set the behaviour
      * @throws NullPointerException
      */
-    public MinimaxSelect(BoardController board, int ID) throws NullPointerException{
-        if(board == null)
+    public MinimaxSelect(BoardController controller, int ID) throws NullPointerException{
+        if(controller == null)
             throw new NullPointerException("MinimaxSelect() : NULL instance of BoardController");
 
-        this.m_board = board;
+        this.m_controller = controller;
         this.m_id = ID;
-        this.m_maxDepth = 6;
+        this.m_maxDepth = 3;
     }
 
     /**
@@ -38,105 +42,120 @@ public class MinimaxSelect implements iSelectable{
      */
     @Override
     public int selectSlot() {
-//        return bestMove();
-        return 0;
+        return findBest() + 1;
     }
 
     /**
-     * Get the best possible move starting from the current state as a tree root
-     * @return X coordinate of the best move found
+     * Find the value in the current game state
+     * @return best value
      */
-/*    private int bestMove(){
-        int best_eval = ERROR;
-        int best = ERROR;
+    private int findBest(){
+        ArrayList<Point> legalShots = new ArrayList<>();
         Point p = new Point(0, 0);
+        for(int i=0 ; i<6 ; i++){
+            p.setCoordinates(i, this.m_id - 1);
+            if (this.m_controller.isLegal(p))
+                legalShots.add(p);
+        }
 
-        //for each of the root children, find the best move
-        BoardController bc = new BoardController();
-        for (int slot=0 ; slot<6 ; slot++){
-            p.setCoordinates(slot, this.m_id - 1);
-            Board b = new Board(this.m_game.getBoardController().getBoard());
-            bc.setBoard(b);
-            int val = evaluate(bc, 0, this.m_id, p, -ERROR);
-            if (val == ERROR)
-                continue;
+        if (legalShots.size() > 0){
+            int bestVal = (this.m_id == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE);
+            for (Point tmp : legalShots){
+                int val = miniMax(this.m_controller, tmp, this.m_maxDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, (this.m_id == 1 ? false : true));
+                if(this.m_id == 1 && val > bestVal)
+                    bestVal = val;
 
-            //if the current tree is the best yet, set it as the best
-            if (val > best_eval) {
-                best_eval = val;
-                best = p.getX();
+                if (this.m_id == 2 && val < bestVal)
+                    bestVal = val;
             }
+            return bestVal;
         }
-        return best + 1;
+        else
+            return -2;
     }
-*/
+
     /**
-     * Evaluate each possible states (tree nodes) recursively, and return the best move
-     * @param depth Depth of the current tree evaluated
-     * @param ID ID of the current player evaluated (maximiser or minimiser)
-     * @param move Coordinates of the current tree
-     * @param alpha Alpha value (Alpha-Beta Pruning)
-     * @return Best move evaluated in the current tree
+     * Use the Minimax algorithm to determine the best move
+     * @param parent BoardController hosting a Board representing the current node of the tree
+     * @param p Slot to select in this node
+     * @param depth Depth of the current node
+     * @param alpha Alpha value in this pruning state
+     * @param beta Beta value in this pruning state
+     * @param maximiser Flag determining if the current node concerns the maximiser or the minimiser (player 2 is the maximiser by default)
+     * @return Best Slot to select
      */
-/*    private int evaluate(BoardController parent, int depth, int ID, Point move, int alpha){
-        //return error if move is not possible
-        if (parent.playSlot(move) < 0)
+    private int miniMax(BoardController parent, Point p, int depth, int alpha, int beta, boolean maximiser){
+        Board buffer = new Board(parent.getBoard());
+        if (parent.playSlot(p) < 0 ) {
+            parent.setBoard(buffer);
             return ERROR;
-
-        //if a leaf has been reached, evaluate the game status
-        if (++depth >= this.m_maxDepth)
-            return evaluateState();
-
-        //invert the ID and prepare variables
-        ID = (ID == 1 ? 2 : 1);
-        int best = ERROR;
-        int val = 0;
-        Point p = new Point(0, 0);
-
-        //for each slot (child nodes), make a copy of the board and evaluate it
-        BoardController bc = new BoardController();
-        for(int slot=0 ; slot<6 ; slot++){
-            Board b = new Board(parent.getBoard());
-            bc.setBoard(b);
-            p.setCoordinates(slot, ID-1);
-            val = evaluate(bc, depth, ID, p, alpha);
-
-            //if the evaluation return an error, process the next child
-            if (val == ERROR)
-                continue;
-
-            //determine if current node is the best one
-            val = -val;
-            if (val > best)
-                best = val;
-            if (val > alpha)
-                break;
         }
-        return best;
+
+        int outcome = evaluateState(parent);
+        if (depth == 0 || outcome == Integer.MAX_VALUE || outcome == Integer.MIN_VALUE ) {
+            return outcome;
+        }
+
+        Point tmp = new Point(0, 0);
+        if (maximiser){
+            int maxEval = Integer.MIN_VALUE;
+            for(int i=0 ; i<6 ; i++){
+                tmp.setCoordinates(i, 1);
+                int eval = miniMax(parent, tmp, depth - 1, alpha, beta, false);
+                maxEval = Math.max(maxEval, eval);
+                alpha = Math.max(alpha, eval);
+                if (beta <= alpha)
+                    break;
+            }
+            parent.setBoard(buffer);
+            return maxEval;
+        }
+        else {
+            int minEval = Integer.MAX_VALUE;
+            for(int i=0 ; i<6 ; i++){
+                p.setCoordinates(i, 0);
+                int eval = miniMax(parent, tmp, depth - 1, alpha, beta, true);
+                minEval = Math.min(minEval, eval);
+                beta = Math.min(beta, eval);
+                if (beta <= alpha)
+                    break;
+            }
+            parent.setBoard(buffer);
+            return minEval;
+        }
     }
-*/
+
     /**
      * Generate an evaluation value for the current state of the game
      * @return Evaluation value
      */
-/*    private int evaluateState(){
+    private int evaluateState(BoardController bc){
         int[] eval = new int[2];
         Point p = new Point(0, 0);
+
+        //if minimiser (player 1) won the game
+        if (this.m_id == 1 && bc.getStoredSeeds(1) > 24)
+            return Integer.MIN_VALUE;
+
+        //if maximiser (player 2) won the game
+        if (this.m_id == 2 && bc.getStoredSeeds(2) > 24)
+            return Integer.MAX_VALUE;
 
         //process the evaluation for both players
         for (int player=0 ; player < 2 ; player++){
             //initialise the evaluation with the amount of seeds captured with a lot of weight
-            eval[player] = 300 * this.m_game.getSeeds(player + 1);
+            eval[player] = 300 * bc.getStoredSeeds(player + 1);
 
             //add the content of the player's slots, each weighted
             for (int slot=0 ; slot < 6 ; slot++) {
                 p.setCoordinates(slot, player);
-                eval[player] += this.m_game.getBoardController().getSlotSeeds(p) * (slot+1);
+                eval[player] += bc.getSlotSeeds(p) * (slot+1);
             }
         }
 
-        //return the delta between both evaluations, the order depending on which player is evaluated
-        return eval[this.m_id - 1] - eval[2 - this.m_id];
+        if(this.m_id == 1)
+            return eval[0] - eval[1];
+        else
+            return eval[1] - eval[0];
     }
-*/
 }
