d1afe16bfb62b189002b6ec0d44f45497add5c7d
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-06 22:35:08 +0200

    create JavaFX properties for slots, names and stored seeds

--- Awele/src/Views/BoardJFXView.java
+++ Awele/src/Views/BoardJFXView.java
@@ -2,6 +2,9 @@ package Views;
 
 import Controllers.BoardController;
 import Controllers.iObserver;
+import Models.Point;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,6 +17,9 @@ import java.io.IOException;
 public class BoardJFXView implements iObserver{
     private Scene m_scene = null;
     private BoardController m_controller;
+    private SimpleIntegerProperty[][] m_slots;
+    private SimpleIntegerProperty[] m_storedSeeds;
+    private SimpleStringProperty[] m_names;
     @FXML Button m_menuButton;
     @FXML Label m_title;
 
@@ -27,6 +33,9 @@ public class BoardJFXView implements iObserver{
         try {
             Parent graph = loader.load();
             this.m_scene = new Scene(graph);
+            this.m_slots = new SimpleIntegerProperty[2][6];
+            this.m_storedSeeds = new SimpleIntegerProperty[2];
+            this.m_names = new SimpleStringProperty[2];
         }
         catch (IOException e){
             System.err.println(e.getMessage());
@@ -38,7 +47,24 @@ public class BoardJFXView implements iObserver{
      * Update nodes values in the scene
      */
     @Override
-    public void update() {}
+    public void update() {
+        //update all the slots with the values from the Board
+        Point p = new Point(0, 0);
+        for (int l=0 ; l<2 ; l++){
+            for (int c=0 ; c<6 ; c++) {
+                p.setCoordinates(c, l);
+                this.m_slots[l][c].set(this.m_controller.getSlotSeeds(p));
+            }
+        }
+
+        //update the stored seeds count for both players
+        this.m_storedSeeds[0].set(this.m_controller.getStoredSeeds(1));
+        this.m_storedSeeds[1].set(this.m_controller.getStoredSeeds(2));
+
+        //update the name of both players
+        this.m_names[0].set(this.m_controller.getName(1));
+        this.m_names[1].set(this.m_controller.getName(2));
+    }
 
     /**
      * Get the current Board scene
