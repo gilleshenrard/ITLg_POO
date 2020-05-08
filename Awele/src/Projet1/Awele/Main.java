58749d6304fc6dcce2529aab8c9548cfc0b7d697
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-08 12:43:22 +0200

    allow user to choose between a JFX or console UI

--- Awele/src/Projet1/Awele/Main.java
+++ Awele/src/Projet1/Awele/Main.java
@@ -1,21 +1,33 @@
 package Projet1.Awele;
 
+import Views.*;
 import javafx.application.Application;
 import Controllers.GameController;
 import Models.Player;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import Views.KeyboardSelect;
-import Views.GameConsoleView;
-import Views.MinimaxSelect;
-import Views.BoardJFXView;
 
 public class Main extends Application{
 
     public static void main(String[] args) {
-        launch(args);
+        switch (args[0]){
+            case "console":
+                startConsole();
+                break;
+            case "jfx":
+                launch(args);
+                break;
+            default:
+                System.err.println("Argument 1 not recognised. (console|jfx)");
+                System.exit(-1);
+                break;
+        }
     }
 
+    /**
+     * Start the game with a JavaFX UI
+     * @param primaryStage Primary JavaFX stage
+     */
     @Override
     public void start(Stage primaryStage) {
         //game setup
@@ -27,14 +39,31 @@ public class Main extends Application{
         game.getBoardController().attach(bv);
 
         //players setup
-        game.setPlayer(new Player(1, "Gilles", new KeyboardSelect()));
+        game.setPlayer(new Player(1, "Gilles", new JFXSelect(game.getBoardController(), 1)));
         game.setPlayer(new Player(2, "AI", new MinimaxSelect(game.getBoardController(), 2)));
 
         //primary Stage setup
         primaryStage.setTitle("POO - Awele");
         primaryStage.setScene((Scene)bv.getContent());
         primaryStage.show();
-/*
+    }
+
+    /**
+     * Start the game with a console UI
+     */
+    public static void startConsole(){
+        //game setup
+        GameController game = new GameController();
+        GameConsoleView gameConsoleView = new GameConsoleView(game);
+
+        //Board setup
+        BoardConsoleView bv = new BoardConsoleView();
+        game.getBoardController().attach(bv);
+
+        //players setup
+        game.setPlayer(new Player(1, "Gilles", new KeyboardSelect()));
+        game.setPlayer(new Player(2, "AI", new MinimaxSelect(game.getBoardController(), 2)));
+
         //state variables
         int outcome = 0;
 
@@ -51,6 +80,5 @@ public class Main extends Application{
         //system error, exit with an error
         if (outcome == -1)
             System.exit(outcome);
-*/
     }
 }
