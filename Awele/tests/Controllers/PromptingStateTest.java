1dc0e3f23c5f57f9cc2f3b7e093251ae13000544
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-19 12:10:53 +0200

    add unit tests

--- Awele/tests/Controllers/PromptingStateTest.java
+++ Awele/tests/Controllers/PromptingStateTest.java
@@ -5,6 +5,7 @@ import Models.Player;
 import Models.Point;
 import Views.BoardView;
 import Views.GameView;
+import Views.KeyboardSelect;
 import Views.RandomSelect;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -50,5 +51,28 @@ public class PromptingStateTest {
         g.setCurrentPlayer(1);
         int ret = g.handleState(6);
         Assertions.assertEquals(6, ret);
+    }
+
+    /**
+     * Check if handleState() processes a self-starvation by scattering to another row
+     */
+    @DisplayName("handleState() with self-starvation to other row, no forfeit - should not fail")
+    @Test
+    void handleState_Forfeit_otherRow_shouldnot_fail() {
+        g.getBoardController().getBoard().setStoredSeeds(1, 0);
+        g.getBoardController().getBoard().setStoredSeeds(2, 0);
+        Game.getInstance().setPlayer(new Player(1, "Test", new KeyboardSelect(g.getBoardController(), 1)));
+        Game.getInstance().setPlayer(new Player(2, "Test", new RandomSelect(g.getBoardController(), 2)));
+        g.getBoardController().getBoard().emptySlotSeeds(new Point(0, 0));
+        g.getBoardController().getBoard().emptySlotSeeds(new Point(1, 0));
+        g.getBoardController().getBoard().emptySlotSeeds(new Point(2, 0));
+        g.getBoardController().getBoard().emptySlotSeeds(new Point(3, 0));
+        g.getBoardController().getBoard().emptySlotSeeds(new Point(4, 0));
+        g.getBoardController().getBoard().emptySlotSeeds(new Point(5, 0));
+        g.getBoardController().getBoard().setRemainingSeeds(1, 0);
+        g.setNextState(GameController.m_prompting);
+        g.setCurrentPlayer(1);
+        int ret = g.handleState(6);
+        Assertions.assertEquals(-2, ret);
     }
 }
--- Awele/tests/Views/KeyboardSelectTest.java
+++ Awele/tests/Views/KeyboardSelectTest.java
@@ -2,12 +2,16 @@ package Views;
 
 import Controllers.BoardController;
 import Models.Board;
+import Models.Player;
+import Models.Point;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class KeyboardSelectTest {
-    KeyboardSelect k = new KeyboardSelect(new BoardController(new Board()), 1);
+    BoardController b = new BoardController(new Board());
+    KeyboardSelect k = new KeyboardSelect(b, 1);
 
     /**
      * Check if selectSlot() properly returns the value set by the user on the keyboard
@@ -18,5 +22,27 @@ class KeyboardSelectTest {
     @Test
     void selectSlot_shouldnot_fail() {
         k.selectSlot();
+    }
+
+    /**
+     * Check if selectSlot() properly returns the value set by the user on the keyboard
+     * This test is disabled until a solution to test keyboard strokes is found
+     */
+    @DisplayName("selectSlot() - should not fail")
+    @Disabled("Need to find a way to emulate user keyboard inputs")
+    @Test
+    void selectSlot_Forfeit_shouldnot_fail() {
+        b.getBoard().setStoredSeeds(1, 0);
+        b.getBoard().setStoredSeeds(2, 0);
+        b.getBoard().emptySlotSeeds(new Point(0, 0));
+        b.getBoard().emptySlotSeeds(new Point(1, 0));
+        b.getBoard().emptySlotSeeds(new Point(2, 0));
+        b.getBoard().emptySlotSeeds(new Point(3, 0));
+        b.getBoard().emptySlotSeeds(new Point(4, 0));
+        b.getBoard().emptySlotSeeds(new Point(5, 0));
+        b.getBoard().setRemainingSeeds(1, 0);
+        Player p = new Player(1, "test", k);
+        int ret = p.selectSlot();
+        Assertions.assertEquals(-2, ret);
     }
 }
