61780cd053560086118afa8d969a6e1511658389
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-23 00:57:16 +0200

    add comments to Launcher

--- Awele/src/com/gilleshenrard/Awele/Launcher.java
+++ Awele/src/com/gilleshenrard/Awele/Launcher.java
@@ -1,6 +1,13 @@
 package com.gilleshenrard.Awele;
 
+/**
+ * Launcher class with a wrapper main method to trick java into thinking this is not a JavaFX application
+ */
 public class Launcher {
+    /**
+     * Wrapper main calling the class extending Application
+     * @param args Program arguments
+     */
     public static void main(String[] args) {
         com.gilleshenrard.Awele.App.main(args);
     }
