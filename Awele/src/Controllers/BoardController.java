27f46b3dbea18c5d5277b43bed0990075659f615
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-26 18:13:07 +0200

    fix starvation issues

--- Awele/src/Controllers/BoardController.java
+++ Awele/src/Controllers/BoardController.java
@@ -196,7 +196,7 @@ public class BoardController {
         if (tmp.getY() == p.getY() || (finalSeeds != 2 && finalSeeds != 3)) {
 
             //if the opponent has no seed left and player doesn't scatter on the opponent's side, starvation
-            if (this.m_board.getRemainingSeeds(2 - p.getY()) == 0 && (p.getX() + nbseeds / 6) <= 0)
+            if (this.m_board.getRemainingSeeds(2 - p.getY()) == 0 && ((p.getX() + nbseeds) / 6) <= 0)
                 return -1;
             else
                 return 0;
