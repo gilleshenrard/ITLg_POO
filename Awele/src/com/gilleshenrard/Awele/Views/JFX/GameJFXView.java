17b4c9e8996f3cd0fb52ebbd29faa7b81f4ff89a
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-27 22:38:37 +0200

    implement JavaFX initialisation and base FXML for the menu pane

--- Awele/src/com/gilleshenrard/Awele/Views/JFX/GameJFXView.java
+++ Awele/src/com/gilleshenrard/Awele/Views/JFX/GameJFXView.java
@@ -11,19 +11,58 @@ import com.gilleshenrard.Awele.App;
 import com.gilleshenrard.Awele.Controllers.GameController;
 import com.gilleshenrard.Awele.Views.iNotifiable;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class GameJFXView implements iNotifiable {
+public class GameJFXView extends GridPane implements Initializable, iNotifiable {
+    private Scene m_scene = null;
+    private GameController m_controller;
+
     /**
      * Create a new Game Java FX view
      * @param controller Game controller to use
      * @throws NullPointerException
      */
     public GameJFXView(GameController controller) throws NullPointerException {
-        controller.setView(this);
+        this.m_controller = controller;
+        this.m_controller.setView(this);
+
+        try {
+            //load the FXML document
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Layouts/MenuSceneGrid.fxml"));
+            loader.setController(this);
+            GridPane graph = loader.load();
+
+            //load custom fonts
+            Font.loadFont(this.getClass().getResource("Styles/FTY_DELIRIUM_NEON_NCV.otf").toExternalForm(), 12);          //-fx-font-family: 'FTY DELIRIUM NEON NCV'
+
+            //create a new scene from the graph
+            this.m_scene = new Scene(graph);
+        }
+        catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Initialise the JavaFX elements of the Menu pane
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //log the main scene initialisation
+        Logger.getLogger(this.getClass().getName()).log(Level.FINE, "Menu scene initialisation");
     }
 
     /**
