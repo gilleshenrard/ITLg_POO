0c4510bcf43a3274d5862fa5c615d1d4c5160ef7
Gilles Henrard <gilles.henrard@hotmail.com>
2020-05-31 00:37:46 +0200

    add toggle group manipulations example

--- Awele/src/com/gilleshenrard/Awele/Views/JFX/GameJFXView.java
+++ Awele/src/com/gilleshenrard/Awele/Views/JFX/GameJFXView.java
@@ -17,6 +17,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -31,7 +33,9 @@ public class GameJFXView extends GridPane implements Initializable, iNotifiable 
     private Stage m_stage;
     private Scene m_menuscene = null;
     private GameController m_controller;
+    private String m_pl1AI;
     @FXML Button b_cancel;
+    @FXML ToggleGroup tg_pl1AI;
 
     /**
      * Create a new Game Java FX view
@@ -43,6 +47,7 @@ public class GameJFXView extends GridPane implements Initializable, iNotifiable 
         this.m_controller = controller;
         this.m_controller.setView(this);
         this.m_stage = stage;
+        this.m_pl1AI = new String();
 
         try {
             //load the FXML document
@@ -71,6 +76,9 @@ public class GameJFXView extends GridPane implements Initializable, iNotifiable 
         //log the main scene initialisation
         Logger.getLogger(this.getClass().getName()).log(Level.FINE, "Menu scene initialisation");
         this.b_cancel.setOnMouseClicked(this::onExitButtonClicked);
+
+        RadioButton tmp_radio = (RadioButton) this.tg_pl1AI.getSelectedToggle();
+        this.m_pl1AI = tmp_radio.getText();
     }
 
     /**
