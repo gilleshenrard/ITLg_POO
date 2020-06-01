0eb97d7ecea340b1fdff059e678dc2fafb485a4a
Gilles Henrard <gilles.henrard@hotmail.com>
2020-06-01 17:57:28 +0200

    set current player to 1 when resetting

--- Awele/src/com/gilleshenrard/Awele/Controllers/GameController.java
+++ Awele/src/com/gilleshenrard/Awele/Controllers/GameController.java
@@ -278,6 +278,7 @@ public class GameController {
      */
     public void resetGame(){
         Game.getInstance().reset();
+        this.setCurrentPlayer(1);
         this.getBoardController().resetBoard();
     }
     /**
